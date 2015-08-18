50d5f9633655644d15f9ae3bbe2b60ae8b9d1c30
Adam Presley <adam@adampresley.com>
2015-08-17 22:49:59 -0500

    Added function to check is the attachment's content is base64 encoded

--- model/attachment/Attachment.go
+++ model/attachment/Attachment.go
@@ -4,6 +4,12 @@
 
 package attachment
 
+import (
+	"math"
+	"regexp"
+	"strings"
+)
+
 type Attachment struct {
 	Id       string            `json:"id"`
 	MailId   string            `json:"mailId"`
@@ -16,4 +22,31 @@ func NewAttachment(headers *AttachmentHeader, contents string) *Attachment {
 		Headers:  headers,
 		Contents: contents,
 	}
+}
+
+/*
+IsContentBase64 returns true/false if the content of this attachment
+resembles a base64 encoded string.
+*/
+func (attachment *Attachment) IsContentBase64() bool {
+	spaceKiller := func(r rune) rune {
+		if r == ' ' || r == '\n' || r == '\r' || r == '\t' {
+			return -1
+		}
+
+		return r
+	}
+
+	trimmedContents := strings.Map(spaceKiller, attachment.Contents)
+
+	if math.Mod(float64(len(trimmedContents)), 4.0) == 0 {
+		matchResult, err := regexp.Match("^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$", []byte(trimmedContents))
+		if err == nil {
+			if matchResult {
+				return true
+			}
+		}
+	}
+
+	return false
 }

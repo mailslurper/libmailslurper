25cf50dc2c7796703370f855172dd03c14103aeb
Adam Presley <adam@adampresley.com>
2015-10-19 15:36:13 -0500

    Corrected misspelling fixing #17

--- storage/Storage.go
+++ storage/Storage.go
@@ -22,7 +22,7 @@ Creates a global connection handle in a map named "lib".
 func ConnectToStorage(connectionInfo *golangdb.DatabaseConnection) error {
 	var err error
 
-	log.Println("libmailslurper: IFNO - Connecting to database", connectionInfo.Database)
+	log.Println("libmailslurper: INFO - Connecting to database", connectionInfo.Database)
 
 	err = connectionInfo.Connect("lib")
 	if err != nil {

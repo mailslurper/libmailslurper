1c68bc7511d46990acd105041896746768f69cfd
Adam Presley <adam@adampresley.com>
2016-02-26 00:49:33 -0600

    Copyright and stuff

--- model/sanitization/XSSServiceProvider.go
+++
@@ -1,9 +0,0 @@
-package sanitization
-
-/*
-XSSServiceProvider is an interface for providing cross-site scripting
-and sanitization services.
-*/
-type XSSServiceProvider interface {
-	SanitizeString(input string) string
-}

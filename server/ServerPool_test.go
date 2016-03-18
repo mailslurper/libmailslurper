74fa0464ed93fdc43bbfe07b7d690160743ed35c
Adam Presley <adam@adampresley.com>
2016-03-17 21:42:20 -0500

    At the suggest of a redditor removing the mock and just using a tcp connection

--- server/ServerPool_test.go
+++ server/ServerPool_test.go
@@ -1,10 +1,10 @@
 package server
 
 import (
+	"net"
 	"testing"
 
 	"github.com/mailslurper/libmailslurper/customerror"
-	"github.com/mailslurper/libmailslurper/mocks"
 	"github.com/mailslurper/libmailslurper/model/mailitem"
 	"github.com/mailslurper/libmailslurper/sanitization"
 	"github.com/mailslurper/libmailslurper/smtpio"
@@ -15,7 +15,6 @@ import (
 func TestServerPool(t *testing.T) {
 	xssService := sanitization.NewXSSService()
 	emailValidationService := sanitization.NewEmailValidationService()
-	mockTCPConnection := mocks.NewMockTCPConn()
 
 	Convey("Creating a new server pool", t, func() {
 		Convey("Returns a ServerPool object", func() {
@@ -36,24 +35,28 @@ func TestServerPool(t *testing.T) {
 
 	Convey("Requesting a worker", t, func() {
 		Convey("With available workers in the pool returns a worker", func() {
+			var err error
+
 			testPool := NewServerPool(1)
 			receiver := make(chan mailitem.MailItem, 10)
+
+			mockConnection := getMockTCPConnection()
 
 			/*
 			 * Create a worker and set it up as if it were receiving a connection
 			 */
 			expected := NewSmtpWorker(1, testPool, emailValidationService, xssService)
 			expected.Prepare(
-				mockTCPConnection,
+				mockConnection,
 				receiver,
-				smtpio.SmtpReader{Connection: mockTCPConnection},
-				smtpio.SmtpWriter{Connection: mockTCPConnection},
+				smtpio.SmtpReader{Connection: mockConnection},
+				smtpio.SmtpWriter{Connection: mockConnection},
 			)
 
 			/*
 			 * Now ask the pool for a worker. It should perform the same steps
 			 */
-			actual, err := testPool.NextWorker(mockTCPConnection, receiver)
+			actual, err := testPool.NextWorker(mockConnection, receiver)
 
 			So(err, ShouldBeNil)
 			So(actual, ShouldResemble, expected)
@@ -63,10 +66,33 @@ func TestServerPool(t *testing.T) {
 			testPool := NewServerPool(0)
 			receiver := make(chan mailitem.MailItem, 10)
 
+			mockConnection := getMockTCPConnection()
+
 			expected := customerror.NoWorkerAvailable()
-			_, actual := testPool.NextWorker(mockTCPConnection, receiver)
+			_, actual := testPool.NextWorker(mockConnection, receiver)
 
 			So(actual, ShouldResemble, expected)
 		})
 	})
+}
+
+func getMockTCPConnection() net.Conn {
+	var err error
+	var mockConnection net.Conn
+	var tcpServer net.Listener
+
+	if tcpServer, err = net.Listen("tcp", "127.0.0.1:0"); err != nil {
+		panic("Unable to create TCP listener on localhost")
+	}
+
+	go func() {
+		defer tcpServer.Close()
+		tcpServer.Accept()
+	}()
+
+	if mockConnection, err = net.Dial("tcp", tcpServer.Addr().String()); err != nil {
+		panic("Unable to dial to mock TCP server")
+	}
+
+	return mockConnection
 }
